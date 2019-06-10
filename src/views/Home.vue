cc5a57ff4e3b297181dc48f3ef95cc7035962f9a
hg.wb.zheng <hg.wb.zheng@gmail.com>
2019-06-10 22:24:38 +0800

    学习vue-typescript用法

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,24 +1,66 @@
 <template>
   <div class="home">
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-    <div>{{name}}</div>
+    <div>{{name | capitalize}}</div>
+    <div>{{allName}}</div>
+    <label for="inp"><input type="text" id="inp" :value="name"></label>
+    <button @click="handleClick">点我</button>
+    <div :key="item.price" v-for="item in books">{{item.name}} - {{item.price}}</div>
+    <div :key="i" v-for="(i, val) in age">{{val}}</div>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import { Component, Vue, Watch } from 'vue-property-decorator';
+import HelloWorld from '@/components/HelloWorld.vue';
+
+// filters
+const capitalize = (value: string) => {
+  if (!value) {
+    return false;
+  }
+  value = value.toString();
+  return value.charAt(0).toUpperCase() + value.slice(1);
+};
 
 @Component({
   components: {
     HelloWorld,
   },
+  filters: { capitalize },
 })
 export default class Home extends Vue {
   private name: string;
+  private books: any[];
+  private age: number[];
   constructor() {
     super();
     this.name = 'bingo keith';
+    this.books = [
+      {
+        name: 'java',
+        price: 11,
+      },
+      {
+        name: 'javascript',
+        price: 22,
+      },
+    ];
+    this.age = [4, 5, 6];
+  }
+  public mounted(): void {
+    console.log('mounted');
+  }
+  public handleClick(e: { target: object }): void {
+    console.log(e.target);
+  }
+  @Watch('name', {immediate: true, deep: true})
+  public onChildChanged(val: string, oldVal: string) {
+    console.log('new name is ' + val);
+  }
+  // computed:计算属性改为前面加get关键字
+  get allName() {
+    return 'computed ' + this.name;
   }
 }
 </script>
