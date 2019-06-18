68621496792e6b69c21c48eff05908867824e3c4
hg.wb.zheng@gmail.com <hg.wb.zheng@gmail.com>
2019-06-18 22:20:05 +0800

    引入MintUI，学习MintUI

--- src/views/Comp.vue
+++ src/views/Comp.vue
@@ -1,6 +1,8 @@
 <template>
 <div>
-  test component
+  <mt-button type="default">default</mt-button>
+  <mt-button type="primary">primary</mt-button>
+  <mt-button type="danger">danger</mt-button>
 </div>
 </template>
 
