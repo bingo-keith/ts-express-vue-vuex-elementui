a2e498ad172ffa9e016b6784e4b541648d573d5d
hg.wb.zheng@gmail.com <hg.wb.zheng@gmail.com>
2019-06-19 21:26:17 +0800

    搭建express服务器，模块化开发

---
+++ server/index.js
@@ -0,0 +1,34 @@
+const path = require('path')
+
+const express = require('express')
+
+const app = express()
+
+const getFiles = require('./routes/server.js')
+
+const users = require('./routes/users.js')
+
+app.use('/', getFiles)
+
+// 模块化开发，通过/users/getList来调用
+app.use('/users', users)
+
+// 获取本机ip地址
+function getIPAdress () {
+    const interfaces = require('os').networkInterfaces();
+    for (const devName in interfaces) {
+        const iface = interfaces[devName];
+        for (const i = 0; i < iface.length; i++) {
+            const alias = iface[i];
+            if (alias.family === 'IPv4' && alias.address !== '127.0.0.1' && !alias.internal) {
+                return alias.address;
+            }
+        }
+    }
+}
+
+const server = app.listen(3000, function () {
+    const host = server.address().address;
+    const port = server.address().port;
+    console.log(`应用实例，访问地址为 http://${getIPAdress()}:${port}`);
+})
