01befe4246d6a5689d148de6ab18853896223412
hg.wb.zheng@gmail.com <hg.wb.zheng@gmail.com>
2019-06-19 19:55:54 +0800

    完成前端框架，技术栈：vue + vuex + vue-router + mint-ui + typescript-

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import MintUI from 'mint-ui';
+import MintUI from 'mint-ui'
+// import 'mint-ui/lib/style.css'
 
 import { currencyFilter } from '@/utils/currency'
-
+console.log(MintUI)
 // 全部引入
 Vue.use(MintUI)
 
