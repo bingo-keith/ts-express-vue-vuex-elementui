5e6cd4b24560f58faaaeb40e4f92484b421dac78
wenbin.zheng <3421736@qq.com>
2019-06-20 10:18:39 +0800

    修复bug

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ function getIPAdress () {
     const interfaces = require('os').networkInterfaces();
     for (const devName in interfaces) {
         const iface = interfaces[devName];
-        for (const i = 0; i < iface.length; i++) {
+        for (let i = 0; i < iface.length; i++) {
             const alias = iface[i];
             if (alias.family === 'IPv4' && alias.address !== '127.0.0.1' && !alias.internal) {
                 return alias.address;
