d558b413afc97f62b93998d66c1f4e3b114e35a9
wenbin.zheng <3421736@qq.com>
2019-12-03 15:45:35 +0800

    add some node config

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,159 @@
+const CompressionWebpackPlugin = require('compression-webpack-plugin')
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const isProduction = process.env.NODE_ENV === 'production'
+
+// cdn预加载使用
+const externals = {
+  vue: 'Vue',
+  'vue-router': 'VueRouter',
+  vuex: 'Vuex',
+  axios: 'axios'
+}
+
+const cdn = {
+  // 开发环境
+  dev: {
+    css: [],
+    js: []
+  },
+  // 生产环境
+  build: {
+    css: [],
+    js: [
+      'https://cdn.bootcss.com/vue/2.6.10/vue.min.js',
+      'https://cdn.bootcss.com/vue-router/3.0.3/vue-router.min.js',
+      'https://cdn.bootcss.com/vuex/3.0.1/vuex.min.js',
+      'https://cdn.bootcss.com/axios/0.19.0/axios.min.js',
+    ]
+  }
+}
+
+// 获取本机ip地址
+function getIPAdress() {
+  var interfaces = require('os').networkInterfaces();
+  for (var devName in interfaces) {
+    var iface = interfaces[devName];
+    for (var i = 0; i < iface.length; i++) {
+      var alias = iface[i];
+      if (alias.family === 'IPv4' && alias.address !== '127.0.0.1' && !alias.internal) {
+        return alias.address;
+      }
+    }
+  }
+}
+
 module.exports = {
+  // 部署生产环境和开发环境下的URL。
+  // 默认情况下，Vue CLI 会假设你的应用是被部署在一个域名的根路径上
+  //例如 https://www.my-app.com/。如果应用被部署在一个子路径上，你就需要用这个选项指定这个子路径。例如，如果你的应用被部署在 https://www.my-app.com/my-app/，则设置 baseUrl 为 /my-app/。
+  publicPath: process.env.NODE_ENV === "production" ? "./" : "/",
+  // outputDir: 在npm run build 或 yarn build 时 ，生成文件的目录名称（要和baseUrl的生产环境路径一致）
+  outputDir: "dist",
+  //用于放置生成的静态资源 (js、css、img、fonts) 的；（项目打包之后，静态资源会放在这个文件夹下）
+  assetsDir: "assets",
+  //指定生成的 index.html 的输出路径  (打包之后，改变系统默认的index.html的文件名)
+  // indexPath: "myIndex.html",
+  //默认情况下，生成的静态资源在它们的文件名中包含了 hash 以便更好的控制缓存。你可以通过将这个选项设为 false 来关闭文件名哈希。(false的时候就是让原来的文件名不改变)
+  filenameHashing: false,
+
+  //   lintOnSave：{ type:Boolean default:true } 问你是否使用eslint
+  lintOnSave: true,
+  //如果你想要在生产构建时禁用 eslint-loader，你可以用如下配置
+  // lintOnSave: process.env.NODE_ENV !== 'production',
+
+  //是否使用包含运行时编译器的 Vue 构建版本。设置为 true 后你就可以在 Vue 组件中使用 template 选项了，但是这会让你的应用额外增加 10kb 左右。(默认false)
+  // runtimeCompiler: false,
+
+  /**
+   * 如果你不需要生产环境的 source map，可以将其设置为 false 以加速生产环境构建。
+   *  打包之后发现map文件过大，项目文件体积很大，设置为false就可以不输出map文件
+   *  map文件的作用在于：项目打包后，代码都是经过压缩加密的，如果运行时报错，输出的错误信息无法准确得知是哪里的代码报错。
+   *  有了map就可以像未加密的代码一样，准确的输出是哪一行哪一列有错。
+   * */
+  productionSourceMap: true,
+  configureWebpack: config => {
+    if (isProduction) {
+      // externals里的模版不打包
+      Object.assign(config, {
+        externals
+      })
+      // 为生产环境修改配置...
+      // 上线压缩去除console等信息
+      config.plugins.push(
+        new UglifyJsPlugin({
+          uglifyOptions: {
+            warnings: false,
+            compress: {
+              drop_console: true,
+              drop_debugger: false,
+              pure_funcs: ['console.log'] // 移除console
+            }
+          },
+          sourceMap: false,
+          parallel: true
+        })
+      )
+      // 开启gzip压缩
+      const productionGzipExtensions = /\.(js|css|json|txt|html|ico|svg)(\?.*)?$/i
+      config.plugins.push(
+        new CompressionWebpackPlugin({
+          filename: '[path].gz[query]',
+          algorithm: 'gzip',
+          test: productionGzipExtensions,
+          threshold: 10240,
+          minRatio: 0.8
+        })
+      )
+      // 打包后模块大小分析//npm run build --report
+      config.plugins.push(new BundleAnalyzerPlugin())
+    } else {
+
+    }
+  },
+  devServer: {
+    open: true, // 启动服务后是否打开浏览器
+    host: getIPAdress(),
+    port: 8080, // 服务端口
+    https: false,
+    hotOnly: false,
+    // proxy: {
+    //   "/api": {
+    //     target: '',
+    //     ws: true,  // 允许跨域
+    //     changeOrigin: true,  //允许跨域
+    //     pathRewrite(path, req) {
+    //       console.log(path, req);
+    //     },
+    //     onProxyReq: function (proxyReq, req, res) {
+    //     }
+    //   },
+    // }
+  },
+  chainWebpack: config => {
+    // 对vue-cli内部的 webpack 配置进行更细粒度的修改。
+    // 添加CDN参数到htmlWebpackPlugin配置中， 详见public/index.html 修改
+    config.plugin('html').tap(args => {
+      if (process.env.NODE_ENV === 'production') {
+        args[0].cdn = cdn.build
+      }
+      if (process.env.NODE_ENV === 'development') {
+        args[0].cdn = cdn.dev
+      }
+      return args
+    })
+    // 设置目录别名alias
+    config.resolve.alias
+      .set('assets', '@/assets')
+      .set('components', '@/components')
+      .set('views', '@/views')
+      .set('servers', '@/servers')
+      .set('utils', '@/utils')
+      .set('store', '@/store')
+  },
   css: {
     // 是否使用css分离插件 ExtractTextPlugin
-    extract: true,
+    extract:isProduction ? true : false,
     // 开启 CSS source maps?
     sourceMap: false,
     // css预设器配置项
