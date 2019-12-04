a31ed48a5ef9c015f6dcb1d6173266210b122cc4
hg.wb.zheng <hg.wb.zheng@gmail.com>
2019-12-04 22:16:31 +0800

    1. fixed axios error about 'Property '$axios' does not exist on type 'App'' 2. unite code style

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,19 @@
-import Vue from 'vue';
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import http from './services/http'
+import Vue from 'vue'
+import App from '@/App.vue'
+import router from '@/router'
+import store from '@/store'
+import http from '@/services/http'
 import { AxiosInstance } from 'axios'
 // import VueAxios from 'vue-axios'
 
-// declare module 'vue/types/vue' {
-//   interface Vue {
-//     $axios: AxiosInstance
-//   }
-//   interface VueConstructor {
-//     $axios: AxiosInstance
-//   }
-// }
+declare module 'vue/types/vue' {
+  interface Vue {
+    $axios: AxiosInstance
+  }
+  interface VueConstructor {
+    $axios: AxiosInstance
+  }
+}
 
 Vue.config.productionTip = false
 
